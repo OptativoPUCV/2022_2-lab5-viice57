7f9687e373719768f1e60bd1c9570a0987c5ab6b
Replit user <>
2022-09-26 00:52:37 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -87,7 +87,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
   if(!searchTreeMap(tree, key)) {
     firstTreeMap(tree);
     
-    while((flag = nextTreeMap(tree))) {
+    while((flag = nextTreeMap(tree)) != NULL) {
       if(tree->lower_than(tree->current->pair->key, key) == 0) break;
     }
 
@@ -107,24 +107,23 @@ Pair * firstTreeMap(TreeMap * tree) {
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
-    if (tree == NULL) return NULL;
-    TreeNode *aux = tree->current;
-    TreeNode *parentNode = aux->parent;
+  if (!tree) return NULL;
+  
+  TreeNode *aux = tree->current;
+  TreeNode *parentNode = aux->parent;
     
-    if (aux->right != NULL) {
-        tree->current = minimum(aux->right);
-    } 
-    else if(aux->right == NULL){
-        while(parentNode != NULL) {
-            if (tree->lower_than(parentNode->pair->key, aux->pair->key) == 1) {
-                parentNode = parentNode->parent;
-                
-            }
-            else 
-                break;
-        }
-        if (parentNode == NULL) return NULL;
-        tree->current = parentNode;
+  if(aux->right) {
+    tree->current = minimum(aux->right);
+  } else {
+    
+    while(parentNode) {
+      if(tree->lower_than(parentNode->pair->key, aux->pair->key)) parentNode = parentNode->parent;
+      else break;
     }
-    return tree->current->pair;
+    
+    if(!parentNode) return NULL;
+    tree->current = parentNode;
+  }
+  
+  return tree->current->pair;
 }
